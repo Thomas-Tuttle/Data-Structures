049afc12cde6f6f1a80fb5509c7fe0ca52c11ef7
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-09-19 13:00:29 -0400

    notes from lecture

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -4,12 +4,25 @@ sys.path.append('../queue_and_stack')
 # from dll_stack import Stack
 
 class BinarySearchTree:
-  def __init__(self, value):
+  def __init__(self, value):  # Using a value so key is value.
     self.value = value
     self.left = None
     self.right = None
-
+# 'inset' adds the input value to the binary search tree, adhearing to the
+# rules of the ordering of elements in a binary search tree
+#Need to traverse th find spot to insert
   def insert(self, value):
+    # if value < self.value
+    #     if not self.left:
+    #         self.left = BinarySearchTree(value)
+    #     else:
+    #         self.left.insert(value)
+    #     else:
+    #     if not self.right:
+    #         self.right = BinarySearchTree(value)
+    #     else:
+    #         self.right.insert(value)          
+
     insert_val = BinarySearchTree(value)
     current_node = self
     placed = False
@@ -27,7 +40,30 @@ class BinarySearchTree:
                 current_node.right = insert_val
                 placed = True
 
+
+
+
+
+
   def contains(self, target):
+      ### Check if the current value is the target, if so done
+    # if self.value == target:
+    #     return True
+    #   ### Otherwise, left or right based on bigger or smaller and then call contains again
+    # else:
+    #     if target < self.value:
+    #         #go left
+    #         if not self.left:
+    #             return False
+    #         else:
+    #             self.left.contains(target)
+    #     else:
+    #         #go right
+    #         if not self.right:
+    #             return False
+    #         else:
+    #             return self.right.contains(target)    
+   
     current_node = self
     found = False
     end = False
@@ -46,7 +82,17 @@ class BinarySearchTree:
             found = True
     return found
 
+
+
+
+# get_max returns the maximum value in the binary search tree.
   def get_max(self):
+#       ### max node is furthest to the right
+#       ### base case:
+#       if not self.right:
+#           return self.value
+#       return self.right.get_max()    
+
     current_node = self
     end = False
     while end == False:
@@ -56,10 +102,19 @@ class BinarySearchTree:
             end = True
     return current_node.value
 
+
+
+# for_each preforms a traversal of _ever_ node in the tree,
+#
+#
+
+
   def for_each(self, cb):
-    if self.value == None:
-        return
     cb(self.value)
+
+    # if self.value == None:
+    #     return
+    
     if self.left:
         self.left.for_each(cb)
     if self.right:
