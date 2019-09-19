9d868a7f3b19d5411f888bd460179b043d1aaab8
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-09-19 11:40:46 -0400

    Update binary_search_tree.py

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+# from dll_queue import Queue
+# from dll_stack import Stack
 
 class BinarySearchTree:
   def __init__(self, value):
@@ -10,13 +10,57 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    insert_val = BinarySearchTree(value)
+    current_node = self
+    placed = False
+    while placed == False:
+        if value < current_node.value:
+            if current_node.left:
+                current_node = current_node.left
+            else:
+                current_node.left = insert_val
+                placed = True
+        if value >= current_node.value:
+            if current_node.right:
+                current_node = current_node.right
+            else:
+                current_node.right = insert_val
+                placed = True
 
   def contains(self, target):
-    pass
+    current_node = self
+    found = False
+    end = False
+    while found == False and end == False:
+        if target < current_node.value:
+            if current_node.left:
+                current_node = current_node.left
+            else:
+                end = True
+        if target > current_node.value:
+            if current_node.right:
+                current_node = current_node.right
+            else:
+                end = True
+        if target == current_node.value:
+            found = True
+    return found
 
   def get_max(self):
-    pass
+    current_node = self
+    end = False
+    while end == False:
+        if current_node.right:
+            current_node = current_node.right
+        else:
+            end = True
+    return current_node.value
 
   def for_each(self, cb):
-    pass
+    if self.value == None:
+        return
+    cb(self.value)
+    if self.left:
+        self.left.for_each(cb)
+    if self.right:
+        self.right.for_each(cb)
